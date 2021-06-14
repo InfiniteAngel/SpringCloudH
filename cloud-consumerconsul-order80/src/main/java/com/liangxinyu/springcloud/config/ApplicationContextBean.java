e7cf1358a04b2563cdba3d984a5d42c86081d489
InfiniteAngel <1067750999@qq.com>
2021-06-14 15:51:40 +0800

    Ribbon

--- cloud-consumerconsul-order80/src/main/java/com/liangxinyu/springcloud/config/ApplicationContextBean.java
+++ cloud-consumerconsul-order80/src/main/java/com/liangxinyu/springcloud/config/ApplicationContextBean.java
@@ -3,13 +3,12 @@ package com.liangxinyu.springcloud.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ApplicationContextConfig {
+public class ApplicationContextBean {
     @Bean
-    //@LoadBalanced
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
